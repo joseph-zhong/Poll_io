af1ce2e81c8a62f6ba4fdd9d44afa5adeec2b19a
ctong1234 <calvinytong@gmail.com>
2015-09-19 02:14:58 -0500

    added parse integration

---
+++ iOS/WUHack/WUHack/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WUHack
+//
+//  Created by Calvin on 9/19/15.
+//  Copyright (c) 2015 Calvin. All rights reserved.
+//
+
+import UIKit
+import Parse
+import Bolts
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
